f5e78fe98bba05f24db68ecb19f81bc3fb1023fd
EkaterinaSirkis <katia212277@gmail.com>
2024-05-05 20:08:31 +0300

    AddedLoginTests

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -5,23 +5,20 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class LoginTests extends TestBase {
-    public class LoginTests extends TestBase{
+
         @BeforeMethod
         public void preCondition(){
-            if(app.getHelperUser().isLogged(){
+            if(app.getHelperUser().isLogged()){
                 app.getHelperUser().logout();
             }
         }
 
         @Test
-    public void loginSuccess(){
+    public void loginSuccess() {
             app.getHelperUser().openLoginForm();
             app.getHelperUser().fillLoginForm("katia212277@gmail.com", "Kotikkk666!");
             app.getHelperUser().submitLogin();
 
             Assert.assertTrue(app.getHelperUser().isLogged());
         }
-
-
     }
-}
